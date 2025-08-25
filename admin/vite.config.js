78e94103b95b9559c412fd7aa7d517309d2ddc17
stinahaziri <stinna.haziri@gmail.com>
2025-08-25 19:32:33 +0200

    d

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -14,8 +14,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Admin/',  
+  plugins: [react()],  
   server: {
     port: 5173
   }
